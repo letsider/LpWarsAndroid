fd1c5a70415fa264841f7f4f91804c62b53d69c4
Cid's computer <Cid's computer@Cidscomputer-HP>
2014-05-27 00:30:21 +0200

    Premiers pas vers notre map généré dynamiquement !
    reprise de ce fichu .gitignore

--- LpWarsAndroid/src/controllers/MainActivity.java
+++ LpWarsAndroid/src/controllers/MainActivity.java
@@ -1,9 +1,8 @@
 package controllers;
 
-import com.example.lpwarsandroid.R;
-
 import models.Carte;
 import models.Gc;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
@@ -11,61 +10,90 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.example.lpwarsandroid.R;
 
 public class MainActivity extends ActionBarActivity {
 
 	private Carte plateauDeJeu = null;
+	ImageButton imageButton;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.fragment_main);
 
-        plateauDeJeu = new Carte(5, new Gc.Couleur[]{Gc.Couleur.bleu, Gc.Couleur.rouge});
+		plateauDeJeu = new Carte(5, new Gc.Couleur[]{Gc.Couleur.bleu, Gc.Couleur.rouge});
+		plateauDeJeu.finTour();
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-    }
+		LayoutInflater vi = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View v = vi.inflate(R.layout.ligne, null);
+		
+		// insert into main view
+		View insertPoint = findViewById(R.id.dynamicFrame);
+		((ViewGroup) insertPoint).addView(v, 0,
+				new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+						ViewGroup.LayoutParams.MATCH_PARENT));
+		
+		addListenerOnButton();
+	}
 
+	public void addListenerOnButton() {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+		imageButton = (ImageButton) findViewById(R.id.imageButton1);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+		imageButton.setOnClickListener(new OnClickListener() {
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
+			@Override
+			public void onClick(View arg0) {
 
-        public PlaceholderFragment() {
-        }
+				Toast.makeText(MainActivity.this,
+						"ImageButton is clicked!", Toast.LENGTH_SHORT).show();
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
+			}
+
+		});
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+			return rootView;
+		}
+	}
 
 }
